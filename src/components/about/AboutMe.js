ea3241bfcf0d08a54beb52e8ac9c8d029cb8df84
Adrian <adrianic92@yahoo.com>
2024-01-28 12:28:13 -0800

    updated about me section

--- src/components/about/AboutMe.js
+++ src/components/about/AboutMe.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import './AboutMe.css';
+import image from '../../images/AboutMePicture.png'
 
 function AboutMe() {
   return (
-    <div>
+    <div className='about-me'>
+      <div className='about-container'>
         <h1 className='title'>About Me</h1>
         <p>Hello all! I'm Adrian Sedano, a recent graduate from Flatiron School, and I'm excited to kick-start my journey in software engineering and development. During my time at Flatiron, I mastered key technologies like JavaScript, React, Ruby, Ruby on Rails, HTML, and CSS. I'm eager to apply these skills in a dynamic tech environment where innovation thrives. <br/><br/>Beyond technical proficiency, my academic journey, including a Bachelor of Arts in Spanish and Linguistics, and a Master of Public Administration, has shaped me into a well-rounded individual. These experiences have not only refined my problem-solving skills but also instilled in me the importance of effective teamwork to achieve common goals. <br/><br/>I am passionate about leveraging my unique blend of technical expertise and diverse academic background to contribute meaningfully to the software engineering landscape. Let's connect and explore how I can bring a fresh perspective and collaborative spirit to your team!</p>
+      </div>
+      <div className='about-image'>
+        <img src={image}/>
+      </div>
     </div>
   )
 }
