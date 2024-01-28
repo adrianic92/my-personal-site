4155e46557b16ce43e813d9fa1bce632447863bd
Adrian <adrianic92@yahoo.com>
2024-01-27 22:42:31 -0800

    updated z-index

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import Home from "./home/Home";
 import NavBar from "./navbar/NavBar";
 import { Route, Routes } from "react-router-dom";
 import './App.css';
+import AboutMe from "./about/AboutMe";
 
 function App() {
   return (
@@ -10,6 +11,9 @@ function App() {
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />}/>
+      </Routes>
+      <Routes>
+        <Route path="/about" element={<AboutMe />}/>
       </Routes>
     </div>
   );
